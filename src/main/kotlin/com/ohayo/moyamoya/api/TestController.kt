fcb0893c570b984e47029abf0465baa720149ab2
hhhello <hhhello0507@dgsw.hs.kr>
2025-01-06 23:56:11 +0900

    에러 테스트

--- src/main/kotlin/com/ohayo/moyamoya/api/TestController.kt
+++ src/main/kotlin/com/ohayo/moyamoya/api/TestController.kt
@@ -1,5 +1,7 @@
 package com.ohayo.moyamoya.api
 
+import com.ohayo.moyamoya.global.CustomException
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController
 class TestController {
     @GetMapping
     fun test() = "Hello"
+    
+    @GetMapping("error")
+    fun errorTest() {
+        throw CustomException(HttpStatus.I_AM_A_TEAPOT, "히히 에러 발싸")
+    }
 }
