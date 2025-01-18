5e8eaa066e4e4b83670f76cc1c747d847eae9524
hhhello <hhhello0507@dgsw.hs.kr>
2025-01-18 22:22:40 +0900

    TimeZone 서울(한국)으로

--- src/main/kotlin/com/ohayo/moyamoya/MoyamoyaApplication.kt
+++ src/main/kotlin/com/ohayo/moyamoya/MoyamoyaApplication.kt
@@ -1,14 +1,22 @@
 package com.ohayo.moyamoya
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
 
 @SpringBootApplication
 @EnableScheduling
 @ConfigurationPropertiesScan
-class MoyamoyaApplication
+class MoyamoyaApplication {
+    @PostConstruct
+    fun init() {
+        // Timezone Fix
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<MoyamoyaApplication>(*args)
